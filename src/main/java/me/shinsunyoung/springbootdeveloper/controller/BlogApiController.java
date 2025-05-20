a7d51790c1a85aa31923eeec1f27249d5fee5af9
SeJunAn <blossom3912@naver.com>
2025-05-20 20:31:05 +0900

    -commit HTTP Response Body에 객체 데이터를 JSON 형식으로 반환하는 컨트롤러

---
+++ src/main/java/me/shinsunyoung/springbootdeveloper/controller/BlogApiController.java
@@ -0,0 +1,11 @@
+package me.shinsunyoung.springbootdeveloper.controller;
+
+import lombok.RequiredArgsConstructor;
+import me.shinsunyoung.springbootdeveloper.service.BlogService;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class BlogApiController {
+
+}
