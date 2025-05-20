4b0cd72a352646b26f606f99e138ba110aa2ab38
SeJunAn <blossom3912@naver.com>
2025-05-20 20:19:37 +0900

    -commit BlogRepository.java 생성

---
+++ src/main/java/me/shinsunyoung/springbootdeveloper/repository/BlogRepository.java
@@ -0,0 +1,7 @@
+package me.shinsunyoung.springbootdeveloper.repository;
+
+import me.shinsunyoung.springbootdeveloper.domain.Article;
+
+public class BlogRepository extends JpaRepository<Article, Long> {
+
+}
