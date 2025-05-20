e49b25159f41d823e65a350f0ed8c2feb81c3d9f
SeJunAn <blossom3912@naver.com>
2025-05-20 20:25:59 +0900

    -commit 빈으로 등록

---
+++ src/main/java/me/shinsunyoung/springbootdeveloper/service/BlogService.java
@@ -0,0 +1,10 @@
+package me.shinsunyoung.springbootdeveloper.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class BlogService {
+
+}
